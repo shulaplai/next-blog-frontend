78b0562220bb2203afecf6afe35f6a290df7422f
Your Name <you@example.com>
2021-10-14 17:37:38 +0800

    Update articles.js

--- components/articles.js
+++ components/articles.js
@@ -8,7 +8,7 @@ const Articles = ({ articles }) => {
 
   return (
     <div>
-      <div className="uk-child-width-2-3@m uk-grid-match" data-uk-grid>
+      <div className="uk-child-width-1-2@s" data-uk-grid="true">
         <div>
           {leftArticles.map((article, i) => {
             return (
@@ -17,7 +17,7 @@ const Articles = ({ articles }) => {
           })}
         </div>
         <div>
-          <div>
+          <div className="uk-child-width-1-2@m uk-grid-match" data-uk-grid>
             {rightArticles.map((article, i) => {
               return (
                 <Cards
