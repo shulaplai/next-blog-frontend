8acf2b46dbf39b1dde828feb31cc4e229cf7f5a9
Your Name <you@example.com>
2021-10-14 13:27:42 +0800

    Update articles.js

--- components/articles.js
+++ components/articles.js
@@ -8,7 +8,7 @@ const Articles = ({ articles }) => {
 
   return (
     <div>
-      <div className="uk-child-width-1-2@s" data-uk-grid="true">
+      <div>
         <div>
           {leftArticles.map((article, i) => {
             return (
