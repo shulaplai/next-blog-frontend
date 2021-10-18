5f096a246c5e1b3079e19d5ebe206594601e53fa
Your Name <you@example.com>
2021-10-18 15:24:09 +0800

    Update articles.js

--- components/articles.js
+++ components/articles.js
@@ -2,11 +2,13 @@ import React from "react"
 import Cards from "./card"
 
 const Articles = ({ articles }) => {
+  const leftArticlesCount = Math.ceil(articles.length / 5)
+  const leftArticles = articles.slice(0, leftArticlesCount)
   return (
     <div>
       <div className=".uk-child-width-*	">
         <div>
-          {Articles.map((article, i) => {
+          {leftArticles.map((article, i) => {
             return (
               <Cards article={article} key={`article__left__${article.slug}`} />
             )
