6672d20e6e8bc635774672ca800caf6164bd2949
Your Name <you@example.com>
2021-11-23 15:55:13 +0800

    change

--- components/articles.js
+++ components/articles.js
@@ -5,15 +5,11 @@ const Articles = ({ articles }) => {
   const Articles = articles.slice(0, articles.length)
   return (
     <div>
-      <div className=".uk-child-width-*	">
-        <div>
-          {Articles.map((article, i) => {
-            return (
-              <Cards article={article} key={`article__left__${article.slug}`} />
-            )
-          })}
-        </div>
-      </div>
+      {Articles.map((article, i) => {
+        return (
+          <Cards article={article} key={`article__left__${article.slug}`} />
+        )
+      })}
     </div>
   )
 }
