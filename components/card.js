ffdd0583f0928518092e919a9c096a742f2f1d72
Your Name <you@example.com>
2021-11-23 16:24:44 +0800

    update

--- components/card.js
+++ components/card.js
@@ -4,12 +4,9 @@ import NextImage from "./image"
 
 const Cards = ({ article }) => {
   return (
-    <div
-      className="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin"
-      uk-grid
-    >
+    <div className="card">
       <Link as={`/article/${article.slug}`} href="/article/[id]" passHref>
-        <div className="uk-card-media-left uk-cover-container uk-card-hover">
+        <div className="container">
           <NextImage
             variant="left"
             image={article.image}
