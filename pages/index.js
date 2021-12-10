b52a90e4a794d2ac5a4c1efeea79563f0ee2ace4
shulaplai <75302349+shulaplai@users.noreply.github.com>
2021-12-10 14:36:24 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import Link from "next/link"
 import Image from "next/image"
 import linkedin_icon from "../assets/css/LinkedIn_logo_initials.png"
 import githubicon from "../assets/css/GitHub-Mark.png"
-import Head from "next/head"
-import { getStrapiMedia } from "../lib/media"
+// import Head from "next/head"
+// import { getStrapiMedia } from "../lib/media"
 const Home = ({ homepage }) => {
   return (
     <div className="intro">
